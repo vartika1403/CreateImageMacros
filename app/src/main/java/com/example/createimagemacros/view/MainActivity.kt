e34abbd6d7a28b7f026e340097ea5d570e0f752b
VARTIKA SHARMA <vartika1403@VARTIKAs-MacBook-Air.local>
2022-11-23 00:10:58 +0530

    added custom view for zoom effect and ontouch listener for moving text view

--- app/src/main/java/com/example/createimagemacros/view/MainActivity.kt
+++ app/src/main/java/com/example/createimagemacros/view/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.createimagemacros.view
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.createimagemacros.databinding.ActivityMainBinding
 import kotlin.random.Random
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(),View.OnTouchListener {
     private lateinit var binding: ActivityMainBinding
     private val SELECT_PICTURE = 200
+    private var xDelta:Float = 0.0F
+    private var yDelta:Float = 0.0F
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +26,9 @@ class MainActivity : AppCompatActivity() {
         binding.selectImageButton.setOnClickListener { imageChooser() }
 
         binding.changeBackgroundButton.setOnClickListener { changeBackGroundColor() }
+
+        binding.editTextButton.setOnClickListener { binding.marocsText.visibility = View.VISIBLE
+        binding.marocsText.setOnTouchListener(this)}
     }
 
     // change random background color
@@ -47,5 +57,25 @@ class MainActivity : AppCompatActivity() {
                    }
                }
            }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouch(view: View?, event: MotionEvent?): Boolean {
+        val x = event?.rawX?.toInt()
+        val y = event?.rawY?.toInt()
+
+        when(event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                if (x != null)
+                    xDelta = view?.x?.minus(x) ?: xDelta
+                if (y != null)
+                    yDelta = view?.y?.minus(y) ?: yDelta
+            }
+            MotionEvent.ACTION_MOVE -> {
+               view?.animate()?.x(event.rawX + xDelta)?.y(event.rawY + yDelta)?.setDuration(0)?.start()
+            }
+            else -> { return false}
+        }
+        return true
     }
 }
